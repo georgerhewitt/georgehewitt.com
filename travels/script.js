2e6e67ff6aa563f988f8c2079e4381796603d51e
George <boyfromtexass@yahoo.com>
2024-11-04 14:07:46 -0600

    Update script.js

--- travels/script.js
+++ travels/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("sample.json")
+  fetch("photos.json")
     .then(response => response.json())
     .then(data => {
       document.getElementById("mainTitle").textContent = data.mainTitle;
